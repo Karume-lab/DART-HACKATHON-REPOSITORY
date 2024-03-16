fc7b59a49188fb1f50b723aea9af4742cecce21f
Karume-lab <danielkarume@outlook.com>
2024-03-16 12:03:32 +0300

    finish Dart Hackathon

---
+++ program1.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "John";
+  int age = 25;
+  String school = "ABC School";
+  String hobby = "Playing guitar";
+  
+  print("My name is $name. I am $age years old and I attend $school. My hobby is $hobby.");
+}
---
+++ program2.dart
@@ -0,0 +1,15 @@
+void main() {
+  int a = 10;
+  int b = 5;
+  
+  print("Sum: ${sum(a, b)}");
+  print("Difference: ${difference(a, b)}");
+}
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+int difference(int a, int b) {
+  return a - b;
+}
